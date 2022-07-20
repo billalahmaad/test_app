2db317d7a7f8a11531c7cd08933d6ed0d4ccf9ac
Bilal Ahmad <bilalahmad.cs.ucp@gmail.com>
2022-07-20 17:52:46 +0500

    CRUD operations using scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'welcome#home'
   get 'about' => 'welcome#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
